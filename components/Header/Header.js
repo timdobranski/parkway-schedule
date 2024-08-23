aa53326105810c81b82d93e39dcf7189b01f1cbe
Tim <timdobranski@gmail.com>
2024-08-22 21:46:29 -0700

    Initial state. Header only

---
+++ components/Header/Header.js
@@ -0,0 +1,16 @@
+'use client';
+
+import styles from './Header.module.css'
+import { useRouter } from 'next/navigation'
+
+export default function Header() {
+  const router = useRouter()
+
+  return (
+    <div className={styles.logoWrapper}>
+      <img src='/photos//parkway.webp'  className={styles.shield} alt="Parkway Logo" onClick={() => router.push('/')}/>
+      <h1 className={styles.title}>PARKWAY SCHEDULE</h1>
+    </div>
+  )
+}
+
